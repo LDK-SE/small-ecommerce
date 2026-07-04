74bdd4027d29c3cb807a57b6832e532f8cc8edd5
LDK-SE <1379469268@qq.com>
2026-07-04 17:53:28 +0800

    Updates

---
+++ assets/usePageMeta-BuhTtLgG.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-BFN_tswI.js";function o(t,a){n.useEffect(()=>{document.title=t;let e=document.querySelector('meta[name="description"]');e||(e=document.createElement("meta"),e.setAttribute("name","description"),document.head.appendChild(e)),e.setAttribute("content",a)},[a,t])}export{o as u};
